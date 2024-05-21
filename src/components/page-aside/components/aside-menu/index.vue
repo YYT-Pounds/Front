924db9bc08e5aa18160ee02ebf6dde92fd52b76c
nbjuzhen/pounds <2028251431@qq.com>
2024-05-21 10:20:30 +0800

    1、完成了动态菜单生成

--- src/components/page-aside/components/aside-menu/index.vue
+++ src/components/page-aside/components/aside-menu/index.vue
@@ -2,23 +2,42 @@
 /**
  * 左侧菜单主体
  */
-import { useRouter } from 'vue-router'
+import {useRouter} from 'vue-router'
 
+/**
+ * 初始化数据
+ */
 const router = useRouter()
-console.log(router)
+const routerList: Array<any> = router.options.routes.slice(1, router.options.routes.length)
+
+/**
+ * 点击
+ */
+const handleClick = (item: any) => {
+  router.push(item.path)
+}
 </script>
 
 <template>
   <div class="aside-menu">
     <el-scrollbar>
-      <p style="color: aliceblue">1</p>
-
+      <div v-for="(item,index) of routerList" :key="index" class="item" @click="handleClick">
+        <span>{{ item.meta.title }}</span>
+      </div>
     </el-scrollbar>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.aside-menu{
+.aside-menu {
+  .item {
+    padding: 15px;
+    cursor: pointer;
 
+    span {
+      color: white;
+      font-size: 18px;
+    }
+  }
 }
 </style>

0d973fa7a410b4a49db670c9bfd8df520a110bc1
nbjuzhen/pounds <2028251431@qq.com>
2024-06-21 17:12:10 +0800

    1、优化了路由引入的方式 2、修复了菜单跳转的问题

--- src/frame/router/index.ts
+++ src/frame/router/index.ts
@@ -1,11 +1,8 @@
 import {createRouter, createWebHashHistory, RouteRecordRaw} from "vue-router";
-
-import login from '@/frame/pages/login/index.vue'
-import index from "@/frame/pages/index/index.vue"
 import routerList from '@/router/index.ts'
-import token from "@/frame/apis/token.ts";
-import request from "@/frame/apis/request.ts";
-import {ElMessage} from "element-plus";
+// import token from "@/frame/apis/token.ts";
+// import request from "@/frame/apis/request.ts";
+// import {ElMessage} from "element-plus";
 
 const routes: RouteRecordRaw[] = [
     {
@@ -37,41 +34,41 @@ const router = createRouter({
     routes: routes
 })
 
-router.beforeEach(async (to, from, next) => {
-    next()
-    return
-    //登录页不鉴权
-    // if (to.name === "login") {
-    //     next()
-    //     return
-    // }
-    // const tokenValue = token.get()
-    // if (tokenValue) {
-    //     try {
-    //         //校验token
-    //         const result = await request.get("")
-    //         //进行判断token有效性，再存入token，无效则跳登录页
-    //         if (result) {
-    //             next()
-    //             ElMessage.success("已为您自动登录")
-    //             return
-    //         } else {
-    //             next({name: "login"})
-    //             token.remove()
-    //             ElMessage.error("登录失效，请重新登录")
-    //             return
-    //         }
-    //     } catch (err) {
-    //         next(false)
-    //         token.remove()
-    //         ElMessage.error("登录失效，请重新登录")
-    //         return
-    //     }
-    // } else {
-    //     //无token，跳转登录页
-    //     next({name: "login"})
-    //     return
-    // }
-})
+// router.beforeEach(async (to, from, next) => {
+//     next()
+//     return
+//     //登录页不鉴权
+//     // if (to.name === "login") {
+//     //     next()
+//     //     return
+//     // }
+//     // const tokenValue = token.get()
+//     // if (tokenValue) {
+//     //     try {
+//     //         //校验token
+//     //         const result = await request.get("")
+//     //         //进行判断token有效性，再存入token，无效则跳登录页
+//     //         if (result) {
+//     //             next()
+//     //             ElMessage.success("已为您自动登录")
+//     //             return
+//     //         } else {
+//     //             next({name: "login"})
+//     //             token.remove()
+//     //             ElMessage.error("登录失效，请重新登录")
+//     //             return
+//     //         }
+//     //     } catch (err) {
+//     //         next(false)
+//     //         token.remove()
+//     //         ElMessage.error("登录失效，请重新登录")
+//     //         return
+//     //     }
+//     // } else {
+//     //     //无token，跳转登录页
+//     //     next({name: "login"})
+//     //     return
+//     // }
+// })
 
 export default router;

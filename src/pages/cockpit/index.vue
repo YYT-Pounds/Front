c8093906f15a40f5e3795a98952148136e7fcf8e
nbjuzhen/pounds <2028251431@qq.com>
2024-06-21 16:46:31 +0800

    1、修复了必填的问题

--- src/pages/cockpit/index.vue
+++ src/pages/cockpit/index.vue
@@ -11,33 +11,33 @@ export default {
 import PageModel from '@/frame/components/page-model/index.vue'
 import {ConfigModel} from "@/frame/model/base/config/config";
 import {ref} from "vue";
-//
-// /**
-//  * 新增
-//  */
-// const handleAdd = () => {
-//   pageModelRef.value.handleAdd()
-// }
-//
-// /**
-//  * 编辑
-//  */
-// const handleEdit = (row: any) => {
-//   pageModelRef.value.handleEdit(row)
-// }
-//
-// /**
-//  * 删除
-//  */
-// const handleDelete = (row: any) => {
-//   pageModelRef.value.handleDelete(row)
-// }
+
+/**
+ * 新增
+ */
+const handleAdd = () => {
+  pageModelRef.value.handleAdd()
+}
+
+/**
+ * 编辑
+ */
+const handleEdit = (row: any) => {
+  pageModelRef.value.handleEdit(row)
+}
+
+/**
+ * 删除
+ */
+const handleDelete = (row: any) => {
+  pageModelRef.value.handleDelete(row)
+}
 
 /**
  * 页面模型
  */
 const pageModelRef = ref()
-const config:ConfigModel = {
+const config: ConfigModel = {
   url: "http://118.178.254.102:1254/peaceful/admin/select",
   method: "get",
   otherParams: {
@@ -45,26 +45,125 @@ const config:ConfigModel = {
   },
   searchForm: [
     {
-      type: "el-input",
+      type: 'el-input',
       prop: "name",
       props: {
-        placeholder: "请输入内容",
+        placeholder: "请输入名称",
+        clearable: true,
       },
-      // renderFn() {
-      //   return <div>员工1号</div>
-      // }
+      renderFn() {
+        return <div>1111</div>
+      }
     },
     {
-      type: "el-input",
+      type: 'el-input',
       prop: "account",
       props: {
-        placeholder: "请输入名字",
-
+        placeholder: "请输入账号",
+        clearable: true
       }
     }
   ],
+  programForm: [
+    {
+      label: "新增",
+      type: 'el-button',
+      props: {
+        type: "primary",
+      },
+      event: handleAdd
+    },
+  ],
   table: {
-    els: []
+    page: {
+      enable: false,
+      pageSize: 999
+    },
+    operation: {
+      width: 160,
+      els: [
+        {
+          type: "el-button",
+          label: "编辑",
+          event: handleEdit,
+          props: {
+            type: "primary",
+            link: true
+          }
+        },
+        {
+          type: "el-button",
+          label: "删除",
+          event: handleDelete,
+          props: {
+            type: "primary",
+            link: true
+          }
+        }
+      ]
+    },
+    props: {
+      stripe: false,
+      border: false,
+    },
+    els: [
+      {
+        label: "用户id",
+        prop: "id",
+        minWidth: 120,
+        renderFn() {
+          return <div>11111</div>
+        }
+      },
+      {
+        label: "用户名",
+        prop: "name",
+        minWidth: 120
+      },
+      {
+        label: "账号",
+        prop: "account",
+        width: 120
+      },
+      {
+        label: "密码",
+        prop: "password",
+        width: 120
+      },
+      {
+        label: "备忘录",
+        prop: "note",
+        width: 120
+      }
+    ]
+  },
+  form: {
+    title: "新增/编辑",
+    labelWidth: "100px",
+    width: "500px",
+    els: [
+      {
+        label: "用户名",
+        type: "el-input",
+        prop: "name",
+        props: {
+          placeholder: "请输入用户名",
+          clearable: true
+        }
+      },
+      {
+        label: "用户名",
+        type: "el-input",
+        prop: "name",
+        props: {
+          placeholder: "请输入用户名",
+          clearable: true
+        },
+        renderFn() {
+          return <div>111</div>
+        }
+      }
+    ]
   }
 }
 </script>
